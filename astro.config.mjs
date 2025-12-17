81044dd1d5fdca2f12657b2c696a2adf161b1ce9
Marianne Feng <feng.marianne@gmail.com>
2025-12-17 16:53:11 -0500

    fix vercel deploy for emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://mariannefeng.com",
+  adapter: vercel(),
   integrations: [mdx(), sitemap()],
 });
